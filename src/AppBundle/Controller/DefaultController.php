bc79df996f6276c247b47ac303502554fbe026fd
Christophe Villeger <christophe.villeger@al-enterprise.com>
2016-07-16 17:00:18 +0200

    back to classic

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,10 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+
+        return $this->render('default/index.html.twig');
     }
 }
