7c79eb94dc971454a47fedb086d89687842d98e2
Christophe Villeger <christophe.villeger@al-enterprise.com>
2016-07-18 20:53:47 +0200

    style

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,18 +5,30 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\User;
 
 class DefaultController extends Controller
 {
     /** @var Request $request */
     protected $request;
+    
+    /** @var User $user */
+    protected $user;
 
     public function preExecute($request){
         $this->request = $request;
+        $this->user = $this->getUser();
     }
 
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        $gravatarHash = 00000000000000000000000000000000;
+        if(!empty($this->user)){
+            $gravatarHash = md5($this->user->getEmailCanonical());
+        }
+
+        return $this->render('default/index.html.twig', [
+            'gravatarHash' => $gravatarHash
+        ]);
     }
 }
